69677fae9aa542e4f4c7ed67845ce4529ae6d376
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-03-22 12:46:59 +0100

    Improvements in library package

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,12 @@ config.output = {
 	library: 'paella-zoom-plugin',
 	libraryTarget: 'umd'
 };
+config.externals = {
+	"paella-core": {
+		commonjs: 'paella-core',
+		commonjs2: 'paella-core',
+		amd: 'paella-core'
+	}
+};
 
 module.exports = config;
