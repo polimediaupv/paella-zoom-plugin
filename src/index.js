eb22b10f10b6e0f6a0f87c4d495e4377855f7e3f
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-04-25 13:54:31 +0200

    New plugins exported

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ZoomCanvas from './plugins/es.upv.paella.zoomPlugin';
 import ZoomInButton from './plugins/es.upv.paella.zoomInButtonPlugin';
 import ZoomOutButton from './plugins/es.upv.paella.zoomOutButtonPlugin';
 import ZoomMenuButton from './plugins/es.upv.paella.zoomMenuButtonPlugin';
+import CanvasZoomInButton from './plugins/es.upv.paella.canvasZoomInButtonPlugin';
+import CanvasZoomOutButton from './plugins/es.upv.paella.canvasZoomOutButtonPlugin';
 
 export default function getZoomPluginContext() {
     return require.context("./plugins", true, /\.js/)
@@ -11,3 +13,5 @@ export const ZoomCanvasPlugin = ZoomCanvas;
 export const ZoomInButtonPlugin = ZoomInButton;
 export const ZoomOutButtonPlugin = ZoomOutButton;
 export const ZoomMenuButtonPlugin = ZoomMenuButton;
+export const CanvasZoomInButtonPlugin = CanvasZoomInButton;
+export const CanvasZoomOutButtonPlugin = CanvasZoomOutButton;
